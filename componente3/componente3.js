0bc97ab76ef4dce91b9c2b87551eff92ef6d47ff
Samuel <sajeronimo@scl.edu.gt>
2024-10-10 11:55:26 -0600

    finalizacion de componentes 3

---
+++ componente3/componente3.js
@@ -0,0 +1,58 @@
+let comp3 = document.createElement('div');
+comp3.className = "comp-3";
+
+let titulo3 = document.createElement('h2');
+titulo3.innerText = "Características";
+comp3.appendChild(titulo3);
+
+let dat1 = document.createElement('h3');
+dat1.innerText = "1. Dato 1";
+dat1.className = "dato-1"
+comp3.appendChild(dat1);
+
+let dat2 = document.createElement('h3');
+dat2.innerText = "2. Dato 2";
+dat2.className = "dato-2"
+comp3.appendChild(dat2);
+
+let dat3 = document.createElement('h3');
+dat3.innerText = "3. Dato 3";
+dat3.className = "dato-3"
+comp3.appendChild(dat3);
+
+let dat4 = document.createElement('h3');
+dat4.innerText = "4. Dato 4";
+dat4.className = "dato-4"
+comp3.appendChild(dat4);
+
+let dat5 = document.createElement('h3');
+dat5.innerText = "5. Dato 5";
+dat5.className = "dato-5"
+comp3.appendChild(dat5);
+
+let dat6 = document.createElement('h3');
+dat6.innerText = "6. Dato 6";
+dat6.className = "dato-6"
+comp3.appendChild(dat6);
+
+let dat7 = document.createElement('h3');
+dat7.innerText = "7. Dato 7";
+dat7.className = "dato-7"
+comp3.appendChild(dat7);
+
+let dat8 = document.createElement('h3');
+dat8.innerText = "8. Dato 8";
+dat8.className = "dato-8"
+comp3.appendChild(dat8);
+
+let dat9 = document.createElement('h3');
+dat9.innerText = "9. Dato 9";
+dat9.className = "dato-9"
+comp3.appendChild(dat9);
+
+let dat10 = document.createElement('h3');
+dat10.innerText = "10. Dato 10";
+dat10.className = "dato-10"
+comp3.appendChild(dat10);
+
+export { comp3 };
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { comp1 } from "./componente1/componente1.js";
 import { comp2 } from "./componente2/componente2.js";
+import { comp3 } from "./componente3/componente3.js";
 
 let root = document.querySelector("#root");
 
@@ -7,5 +8,6 @@ let cuadro1 = document.createElement('div');
 cuadro1.className = "cuadro";
 cuadro1.appendChild(comp1);
 cuadro1.appendChild(comp2);
+cuadro1.appendChild(comp3);
 
 root.appendChild(cuadro1);
